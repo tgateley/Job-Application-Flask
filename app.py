6a60618f55e14ecaf26c99aaf835407e900345c9
Tiffany <tgateley22@gmail.com>
2024-09-18 11:27:47 -0500

    Send Conformation email to the user

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from datetime import datetime
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "myapplication123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "----Email Here-----"
+app.config["MAIL_PASSWORD"] = "------YOUR EMAIL PASSWORD GOES HERE----"
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -32,6 +40,16 @@ def index():
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = (f"Thank you for your submission, {first_name}."
+                        f"Here is your information that was submitted:\n"
+                        f"{first_name} {last_name} \n {date}\n"
+                        f" Thank you!")
+        message = Message(subject="New Form Submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
         flash(f"{first_name}, your form was submitted successfully!", "success")
 
     return render_template("index.html")
